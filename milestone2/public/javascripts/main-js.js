ee9c1281dc30f00d205cef3435fc1a3189e2111b
Carlos Atis <carlos.atis154@gmail.com>
2020-05-29 18:25:05 +0930

    modified main.html

--- milestone2/public/javascripts/main-js.js
+++ milestone2/public/javascripts/main-js.js
@@ -26,3 +26,15 @@ $(document).ready(function() {
   // Automatically add a first row of data
   $('#addRow').click();
 });
+
+//date datepicker
+$(document).ready(function(){
+		var date_input=$('input[name="date"]'); //our date input has the name "date"
+		var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+		date_input.datepicker({
+			format: 'mm/dd/yyyy',
+			container: container,
+			todayHighlight: true,
+			autoclose: true,
+		})
+	});
