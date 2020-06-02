d1d49f361f3fd567cb76e12d53aa59c6967029fa
Carlos Atis <carlos.atis154@gmail.com>
2020-06-02 14:56:07 +0930

    F

--- milestone2/public/javascripts/main-js.js
+++ milestone2/public/javascripts/main-js.js
@@ -41,103 +41,101 @@ var vuemain = new Vue({
       manager_name: "Carlos Atis",
       manager_image: "images/smiley.jfif"
     },
-    task_tags: ["Cleaning", "Studying", "Research", "Documenting"],
+    task_types: ["Cleaning", "Studying", "Research", "Documenting"],
 
-    task_performance: ["Good", //15 length as long as task tags
-      "Great",
-      "Unsatisfactory",
-      "Not Set",
+    task_performance: [{
+        performance: "Good",
+        color: "good"
+      }, //15 length as long as task tags
+      {
+        performance: "Great",
+        color: "great"
+      },
+      {
+        performance: "Unsatisfactory",
+        color: "unsatisfactory"
+      },
+      {
+        performance: "Not Set",
+        color: "not-set"
+      },
     ],
 
     availability: {
-      mon_s: "0800",
-      mon_e: "2000",
-      tue_s: "0800",
-      tue_e: "2000",
-      wed_s: "0800",
-      wed_e: "2000",
-      thu_s: "0800",
-      thu_e: "2000",
-      fri_s: "0800",
-      fri_e: "2000",
-      sat_s: "0000",
-      sat_e: "2000",
-      sun_s: "0000",
-      sun_e: "2000"
+      mon_s: "08:00",
+      mon_e: "20:00",
+      tue_s: "08:00",
+      tue_e: "20:00",
+      wed_s: "08:00",
+      wed_e: "20:00",
+      thu_s: "08:00",
+      thu_e: "20:00",
+      fri_s: "08:00",
+      fri_e: "20:00",
+      sat_s: "00:00",
+      sat_e: "20:00",
+      sun_s: "00:00",
+      sun_e: "20:00"
     },
 
     members: [{
         name: "Carlos Atis",
         image: "somesource",
         availability: {
-          mon_s: "0600",
-          mon_e: "2000",
-          tue_s: "0800",
-          tue_e: "2000",
-          wed_s: "0800",
-          wed_e: "2000",
-          thu_s: "0800",
-          thu_e: "2000",
-          fri_s: "0800",
-          fri_e: "2000",
-          sat_s: "0000",
-          sat_e: "2000",
-          sun_s: "0000",
-          sun_e: "2000"
+          mon_s: "06:00",
+          mon_e: "20:00",
+          tue_s: "08:00",
+          tue_e: "20:00",
+          wed_s: "08:00",
+          wed_e: "20:00",
+          thu_s: "08:00",
+          thu_e: "20:00",
+          fri_s: "08:00",
+          fri_e: "20:00",
+          sat_s: "00:00",
+          sat_e: "20:00",
+          sun_s: "00:00",
+          sun_e: "20:00"
         },
-          task_performance: ["Good",
+        task_performance: ["Good",
           "Great",
           "Unsatisfactory",
           "Not Set",
-        ] //15 length as long as task tags
+        ],
+        task_performance_string: "Cleaning: Good \nStudying: Good \nResearch: Great \nDocumenting: Unsatisfactory" //15 length as long as task tags
       },
 
       {
         name: "Huatao Dong",
         image: "images/yellow-flower.jpg",
         availability: {
-          mon_s: "0800",
-          mon_e: "2000",
-          tue_s: "0800",
-          tue_e: "2000",
-          wed_s: "0800",
-          wed_e: "2000",
-          thu_s: "0800",
-          thu_e: "2000",
-          fri_s: "0800",
-          fri_e: "2000",
-          sat_s: "0000",
-          sat_e: "2000",
-          sun_s: "0000",
-          sun_e: "2000"
+          mon_s: "08:00",
+          mon_e: "20:00",
+          tue_s: "08:00",
+          tue_e: "20:00",
+          wed_s: "08:00",
+          wed_e: "20:00",
+          thu_s: "08:00",
+          thu_e: "20:00",
+          fri_s: "08:00",
+          fri_e: "20:00",
+          sat_s: "00:00",
+          sat_e: "20:00",
+          sun_s: "00:00",
+          sun_e: "20:00"
         },
         task_performance: ["Good",
           "Great",
           "Unsatisfactory",
           "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set",
-          "Not Set", //15 length
         ],
+        task_performance_string: "Cleaning: Good \nStudying: Great \nResearch: Unsatisfactory \nDocumenting: Not set"
       }
     ],
     tasks: [{
       ticket: 1,
       task_name: "Research",
-      task_tag: "Study",
+      task_type: "Study",
       start_time: "08:00",
       end_time: "10:00",
       status: "Complete",
@@ -146,7 +144,7 @@ var vuemain = new Vue({
     }, {
       ticket: 2,
       task_name: "Watering plants",
-      task_tag: "Gardening",
+      task_type: "Gardening",
       start_time: "19:00",
       end_time: "20:00",
       status: "Complete",
@@ -156,13 +154,135 @@ var vuemain = new Vue({
 
   },
   methods: {
-    add_task_tag: function() {},
-    add_member: function() {},
-    submit_availability: function() {},
+    add_task_type: function() {
+      var task_type_value = document.getElementById("task_type_input").value;
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+
+      xhttp.open("POST", "/manager/add_task_type", true);
+      xhttp.send(task_type_value);
+    },
+
+    add_member: function() {
+      var member_email_value = document.getElementById("member-email-input").value;
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+      xhttp.open("POST", "/manager/add_member", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(member_email_value);
+    },
+
+    add_task: function() {
+      // var task_name_value = document.getElementById("member-email-input").value;
+      // var task_tag_value= document.getElementById("member-email-input").value;
+      // var task_start_time_value= document.getElementById("member-email-input").value;
+      // var task_end_time_value= document.getElementById("member-email-input").value;
+      // var task_persons=[];
+      // var task_priority_value= document.getElementById("member-email-input").value;
+
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+
+      xhttp.open("POST", "/manager/add_task", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        "task_name": task_name_value,
+        "task_tag": task_tag_value,
+        "task_start_time": task_start_time_value,
+        "task_end_time": task_end_time_value,
+        "task_persons": task_persons,
+        "task_priority": task_priority_value
+      }));
+    },
+
+    edit_task: function() {
+      // var task_name_value = document.getElementById("member-email-input").value;
+      // var task_tag_value= document.getElementById("member-email-input").value;
+      // var task_start_time_value= document.getElementById("member-email-input").value;
+      // var task_end_time_value= document.getElementById("member-email-input").value;
+      //  var task_status= document.getElementById("member-email-input").value;
+      // var task_persons=[];
+      // var task_priority_value= document.getElementById("member-email-input").value;
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+      xhttp.open("POST", "/manager/edit_task", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        "task_name": task_name_value,
+        "task_tag": task_tag_value,
+        "task_start_time": task_start_time_value,
+        "task_end_time": task_end_time_value,
+        "task_status": task_status_value,
+        "task_persons": task_persons,
+        "task_priority": task_priority_value
+      }));
+    },
+
+    finish_task: function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+      xhttp.open("POST", "/manager/finish_task", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        lines: lines,
+        suffix: suffix
+      }));
+    },
+
+    save_availability: function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+      xhttp.open("POST", "/user/save_availability", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        lines: lines,
+        suffix: suffix
+      }));
+    },
+
+    set_performance: function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+      };
+      xhttp.open("POST", "/user/set_performance", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        lines: lines,
+        suffix: suffix
+      }));
+    }
   },
 
-  computed:{
-    availability_string:function(){
+  computed: {
+    availability_string: function() {
       var count = 0;
       return this.members[count].availability.mon_s;
       count++;
@@ -170,7 +290,23 @@ var vuemain = new Vue({
   }
 })
 
-//highligh active board
+//---------------------default values for availability sliders in  'My availability modal'---------------------
+var my_availability = {
+  mon_s: "08:00",
+  mon_e: "20:00",
+  tue_s: "08:00",
+  tue_e: "20:00",
+  wed_s: "08:00",
+  wed_e: "20:00",
+  thu_s: "08:00",
+  thu_e: "20:00",
+  fri_s: "08:00",
+  fri_e: "20:00",
+  sat_s: "00:00",
+  sat_e: "20:00",
+  sun_s: "00:00",
+  sun_e: "20:00"
+}
 
 
 
@@ -206,19 +342,6 @@ $(document).ready(function() {
   $('#addRow').click();
 });
 
-
-
-//---------------------data picker---------------------
-$(document).ready(function() {
-  var date_input = $('input[name="date"]'); //our date input has the name "date"
-  var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-  date_input.datepicker({
-    format: 'mm/dd/yyyy',
-    container: container,
-    todayHighlight: true,
-    autoclose: true,
-  })
-});
 
 //---------------------popover---------------------
 $(function() {
