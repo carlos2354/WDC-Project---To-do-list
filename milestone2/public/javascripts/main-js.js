05dfe8f90f996ca041800d523e1a926f4ef2ff7d
Carlos Atis <carlos.atis154@gmail.com>
2020-06-04 20:55:11 +0930

    g

--- milestone2/public/javascripts/main-js.js
+++ milestone2/public/javascripts/main-js.js
@@ -2,12 +2,14 @@ var vuemain = new Vue({
   el: '#main',
   data: {
     user: {
-      info:{
+      info: {
         id: "000001",
         name: "Carlos Atis",
+        first_name:"Carlos",
+        last_name:"Atis",
         email: "carlos.atis154@gmail.com",
         profilePicture: "images/pngfuel.com.png",
-        birthday: "Oct 1, 1998",
+        birthday: "01/10/1998",
         phone: 0469863752,
         background: "student",
         isManager: false,
@@ -45,62 +47,68 @@ var vuemain = new Vue({
       manager_name: "Carlos Atis",
       manager_image: "images/smiley.jfif"
     },
+
     task_types: ["Cleaning", "Studying", "Research", "Documenting"],
 
     task_performance: [{
+        type: "Cleaning",
         performance: "Good",
         color: "good"
       }, //15 length as long as task tags
       {
+        type: "Studying",
         performance: "Great",
         color: "great"
       },
       {
+        type: "Research",
         performance: "Unsatisfactory",
         color: "unsatisfactory"
       },
       {
+        type: "Documenting",
         performance: "Not Set",
         color: "not-set"
       },
     ],
 
     availability: {
-      mon_s: "08:00",
-      mon_e: "20:00",
-      tue_s: "08:00",
-      tue_e: "20:00",
-      wed_s: "08:00",
-      wed_e: "20:00",
-      thu_s: "08:00",
-      thu_e: "20:00",
-      fri_s: "08:00",
-      fri_e: "20:00",
-      sat_s: "00:00",
-      sat_e: "20:00",
-      sun_s: "00:00",
-      sun_e: "20:00"
+      mon_s: 8,
+      mon_e: 20,
+      tue_s: 8,
+      tue_e: 20,
+      wed_s: 8,
+      wed_e: 20,
+      thu_s: 8,
+      thu_e: 20,
+      fri_s: 8,
+      fri_e: 20,
+      sat_s: 8,
+      sat_e: 20,
+      sun_s: 8,
+      sun_e: 20,
     },
 
     members: [{
-        name: "Carlos Atis",
-        image: "somesource",
+        first_name:"Carlos",
+        last_name:"Atis",
+        image: "images/pngfuel.com.png",
         id: "000001",
         availability: {
-          mon_s: "06:00",
-          mon_e: "20:00",
-          tue_s: "08:00",
-          tue_e: "20:00",
-          wed_s: "08:00",
-          wed_e: "20:00",
-          thu_s: "08:00",
-          thu_e: "20:00",
-          fri_s: "08:00",
-          fri_e: "20:00",
-          sat_s: "00:00",
-          sat_e: "20:00",
-          sun_s: "00:00",
-          sun_e: "20:00"
+          mon_s: 6,
+          mon_e: 20,
+          tue_s: 8,
+          tue_e: 20,
+          wed_s: 7,
+          wed_e: 23,
+          thu_s: 10,
+          thu_e: 15,
+          fri_s: 6,
+          fri_e: 12,
+          sat_s: 8,
+          sat_e: 20,
+          sun_s: 8,
+          sun_e: 20
         },
         task_performance: ["Good",
           "Great",
@@ -111,24 +119,25 @@ var vuemain = new Vue({
       },
 
       {
-        name: "Huatao Dong",
+        first_name: "Huatao",
+        last_name: "Dong",
         id: "000002",
         image: "images/yellow-flower.jpg",
         availability: {
-          mon_s: "08:00",
-          mon_e: "20:00",
-          tue_s: "08:00",
-          tue_e: "20:00",
-          wed_s: "08:00",
-          wed_e: "20:00",
-          thu_s: "08:00",
-          thu_e: "20:00",
-          fri_s: "08:00",
-          fri_e: "20:00",
-          sat_s: "00:00",
-          sat_e: "20:00",
-          sun_s: "00:00",
-          sun_e: "20:00"
+          mon_s: 8,
+          mon_e: 20,
+          tue_s: 10,
+          tue_e: 20,
+          wed_s: 10,
+          wed_e: 23,
+          thu_s: 10,
+          thu_e: 15,
+          fri_s: 8,
+          fri_e: 20,
+          sat_s: 9,
+          sat_e: 20,
+          sun_s: 10,
+          sun_e: 18
         },
         task_performance: ["Good",
           "Great",
@@ -139,7 +148,7 @@ var vuemain = new Vue({
       }
     ],
     tasks: [{
-        ticket: 1,
+        id: 1,
         name: "Research",
         type: "Study",
         start_time: "08:00",
@@ -148,7 +157,7 @@ var vuemain = new Vue({
         person: ["Carlos", "Hunter"],
         priority: "high"
       }, {
-        ticket: 2,
+        id: 2,
         name: "Watering plants",
         type: "Gardening",
         start_time: "19:00",
@@ -157,7 +166,7 @@ var vuemain = new Vue({
         priority: "medium"
       },
       {
-        ticket: 3,
+        id: 3,
         name: "Studying Code",
         type: "Study",
         start_time: "15:00",
@@ -176,11 +185,14 @@ var vuemain = new Vue({
 
     //----------------------------general ajax----------------------------
     populate_today: function(id) {
-      // var userID = from cookie
       vuemain.user.table_display = false;
       vuemain.user.profile_display = true;
+
       var board_id_value = id;
-      var date_value = ""; //today's date
+      var today = new Date;
+      var year = today.getFullYear();
+      var month = today.getMonth();
+      var day = today.getDate();
 
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
@@ -189,13 +201,22 @@ var vuemain = new Vue({
         }
       };
 
-      xhttp.open("GET", "/users/board/?board_id=" + encodeURIComponent(board_id_value) + "&date=" + encodeURIComponent(date_value), true);
-      xhttp.send();
+      xhttp.open("POST", "/users/board/populate", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        "day": day,
+        "month": month,
+        "year": year,
+      }));
     },
 
     populate_on_date: function() {
       var board_id_value = vuemain.board.id;
-      var date_value = ""; //selected date
+
+      var year = "";
+      var month = "";
+      var day = "";
+
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -203,17 +224,18 @@ var vuemain = new Vue({
         }
       };
 
-      xhttp.open("GET", "/users/board?board_id=" + encodeURIComponent(board_id_value) + "&date=" + encodeURIComponent(date_value), true);
-      xhttp.send();
+      xhttp.open("POST", "/users/board/populate", true);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        "day": day,
+        "month": month,
+        "year": year,
+      }));
     },
 
     save_profile: function() {
-      var i_name ="";
-      var i_email = "";
-      var i_birthday = "";
-      var i_phone = "";
-      var i_background ="";
 
+      var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           vuemain.user.info = JSON.parse(this.response);
@@ -223,17 +245,18 @@ var vuemain = new Vue({
       xhttp.open("POST", "/users/save_profile", true);
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({
-        "name": i_name,
-        "email": i_email,
-        "birthday": i_birthday,
-        "phone": i_phone,
-        "background": i_background,
+        "first_name": vuemain.user.info.first_name,
+        "last_name":vuemain.user.info.last_name,
+        "email":vuemain.user.info.email,
+        "birthday": vuemain.user.info.birthday,
+        "phone": vuemain.user.info.phone,
+        "background":  vuemain.user.info.background,
       }));
     },
 
     //----------------------------manager ajax----------------------------
     add_task_type: function() {
-      var task_type_value = document.getElementById("task-type-input").value;
+      var task_type_value = document.getElementById("task-type-create").value;
 
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
@@ -244,6 +267,8 @@ var vuemain = new Vue({
 
       xhttp.open("POST", "/users/manager/add_task_type", true);
       xhttp.send(task_type_value);
+
+      document.getElementById("task-type-create").value = "";
     },
 
     add_member: function() {
@@ -257,16 +282,27 @@ var vuemain = new Vue({
       };
       xhttp.open("POST", "/users/manager/add_member", true);
       xhttp.send(member_email_value);
+
+      document.getElementById("member-email-input").value = "";
     },
 
     add_task: function() {
-      // var task_name_value = document.getElementById("member-email-input").value;
-      // var task_tag_value= document.getElementById("member-email-input").value;
-      // var task_start_time_value= document.getElementById("member-email-input").value;
-      // var task_end_time_value= document.getElementById("member-email-input").value;
-      // var task_persons=[];
-      // var task_priority_value= document.getElementById("member-email-input").value;
+      var task_name_value = document.getElementById("member-email-input").value;
+      var task_type_select = document.getElementById("task_type_select");
+      var task_type_selected = task_type_select.options[task_type_select.selectedIndex].text;
+      var task_start_time_value= parseInt(document.getElementById("task_start").innerHTML);
+      var task_end_time_value= parseInt(document.getElementById("task_end").innerHTML);
+      var task_persons=$(".member-list-checkbox:checked");
+      var task_persons_value=[];
+      var task_priority_value= $(".priority-radio:checked").val();
 
+      for(var i = 0; i <task_persons.length; i++)
+      {
+        task_persons_value.push(task_persons[i].value);
+        // console.log(task_persons_value[0]);
+      }
+
+      var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           vuemain.tasks = JSON.parse(this.response);
@@ -277,45 +313,37 @@ var vuemain = new Vue({
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({
         "name": task_name_value,
-        "tag": task_tag_value,
+        "tag": task_type_selected,
         "start_time": task_start_time_value,
         "end_time": task_end_time_value,
-        "persons": task_persons,
+        "persons": task_persons_value,  //sends full name instead of first and last
         "priority": task_priority_value
       }));
     },
 
-    finish_task: function(index) {
-      var task_ticket_value = document.getElementById("task-ticket");
+    finish_task: function(id) {
 
-      alert(index);
+      console.log(id);
+      
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           vuemain.tasks = JSON.parse(this.response);
         }
       };
+
       xhttp.open("POST", "/users/manager/finish_task", true);
-      xhttp.send(ticket);
+      xhttp.setRequestHeader("Content-type", "application/json");
+      xhttp.send(JSON.stringify({
+        "task_id": id,
+        "board_id": vuemain.board.id,
+      }));
     },
 
     //----------------------------employee ajax----------------------------
     save_availability: function() {
-      var sun_start_value = document.getElementById("sundayfro").value;
-      var sun_end_value = document.getElementById("sundayto").value;
-      var mon_start_value = document.getElementById("mondayfro").value;
-      var mon_end_value = document.getElementById("mondayto").value;
-      var tue_start_value = document.getElementById("tuesdayfro").value;
-      var tue_end_value = document.getElementById("tuesdayto").value;
-      var wed_start_value = document.getElementById("wednesdayfro").value;
-      var wed_end_value = document.getElementById("wednesdayto").value;
-      var thu_start_value = document.getElementById("thursdayfro").value;
-      var thu_end_value = document.getElementById("thursdayto").value;
-      var fri_start_value = document.getElementById("fridayfro").value;
-      var fri_end_value = document.getElementById("fridayto").value;
-      var sat_start_value = document.getElementById("saturdayfro").value;
-      var sat_end_value = document.getElementById("saturdayto").value;
-
+      console.log(vuemain.availability.sun_s);
+      console.log(vuemain.availability.sun_e);
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -326,27 +354,26 @@ var vuemain = new Vue({
       xhttp.open("POST", "/users/user/save_availability", true);
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({
-        "sun_s": sun_start_value,
-        "sun_e": sun_end_value,
-        "mon_s": mon_start_value,
-        "mon_e": mon_end_value,
-        "tue_s": tue_start_value,
-        "tue_e": tue_end_value,
-        "wed_s": wed_start_value,
-        "wed_e": wed_end_value,
-        "thu_s": thu_start_value,
-        "thu_e": thu_end_value,
-        "fri_s": fri_start_value,
-        "fri_e": fri_end_value,
-        "sat_s": sat_start_value,
-        "sat_e": sat_end_value,
+        "sun_s": vuemain.availability.sun_s,
+        "sun_e": vuemain.availability.sun_e,
+        "mon_s": vuemain.availability.mon_s,
+        "mon_e": vuemain.availability.mon_e,
+        "tue_s": vuemain.availability.tue_s,
+        "tue_e": vuemain.availability.tue_e,
+        "wed_s": vuemain.availability.wed_s,
+        "wed_e": vuemain.availability.wed_e,
+        "thu_s": vuemain.availability.thu_s,
+        "thu_e": vuemain.availability.thu_e,
+        "fri_s": vuemain.availability.fri_s,
+        "fri_e": vuemain.availability.fri_e,
+        "sat_s": vuemain.availability.sat_s,
+        "sat_e": vuemain.availability.sat_e,
       }));
     },
 
-    set_performance: function() {
-      var task_performance_value = "";
-      var task_index = "";
-      var task_name_value = "";
+    set_performance: function(performance, index) {
+      var performance_value = performance;
+      var index_value = index;
 
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
@@ -354,12 +381,12 @@ var vuemain = new Vue({
           vuemain.task_performance = this.response;
         }
       };
+
       xhttp.open("POST", "/users/user/set_performance", true);
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({
-        "index": task_index,
-        "name": name,
-        "performance": performance,
+        "performance": performance_value,
+        "index": index_value,
       }));
     }
   },
